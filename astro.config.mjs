42bc4a8a59195a16c81b66ff547b946894473587
camilo guzman <jeyonkmguzman@gmail.com>
2024-05-26 18:34:20 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: cloudflare()
+  adapter: vercel()
 });
